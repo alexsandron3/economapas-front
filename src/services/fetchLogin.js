559ee8a304cd6351ed7d583a5ed6796be981a752
alexsandron3 <alexsandro060299@outlook.com>
2021-11-21 15:53:59 -0300

    Cria função para requisição de login

---
+++ src/services/fetchLogin.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const API_URL =
+  'http://localhost/desafios/economapas/economapas-API/api/user.php';
+export const fetchLogin = async ({ username, password }) => {
+  const { data } = await axios({
+    method: 'POST',
+    url: API_URL,
+    data: { username, password },
+  });
+
+  return { data };
+};
