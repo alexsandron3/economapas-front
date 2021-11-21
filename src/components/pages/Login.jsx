09371c948c014c3fa4d3e2485f1a4aa862606831
alexsandron3 <alexsandro060299@outlook.com>
2021-11-21 15:18:07 -0300

    Cria rota para login

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return 1;
+  }
+}
+
+export default Login;
